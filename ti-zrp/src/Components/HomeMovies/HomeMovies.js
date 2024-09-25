9da714bebeebdd43acf110660c7b2ef255709ac3
TZippilli <tzippilli@udesa.edu.ar>
2024-09-24 22:28:48 -0300

    format y estilo

--- ti-zrp/src/Components/HomeMovies/HomeMovies.js
+++ ti-zrp/src/Components/HomeMovies/HomeMovies.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react'
 import MovieGrid from '../MovieGrid/MovieGrid'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export class HomeMovies extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            movieList: []
-        }
+    this.state = {
+      movieList: []
     }
+  }
 
+  componentDidMount() {
+    fetch(this.props.url)
+      .then((response) => response.json())
+      .then((movies) => this.setState({ movieList: movies.results.slice(0, 5) }))
+      .catch((err) => console.log(err))
+  }
 
-    componentDidMount() {
-        fetch(this.props.url).
-        then((response) => response.json()).
-        then((movies) => this.setState({ movieList: movies.results.slice(0, 5) })).
-        catch((err) => console.log(err))
-    }
-
-    render() {
-        return (
-            <div>
-            {this.state.movieList.length === 0 ? <img src="./gifLoader.gif" alt="loader" ></img> : <MovieGrid pelis={this.state.movieList} /> }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.state.movieList.length === 0 ? <img src="./gifLoader.gif" alt="loader" ></img> : <MovieGrid pelis={this.state.movieList} />}
+      </div>
+    )
+  }
 }
 
 export default HomeMovies
